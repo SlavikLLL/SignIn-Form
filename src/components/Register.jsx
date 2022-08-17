e010992db9965d14819d45f4f3cfe5405d321807
SlavikLLL <vyacheslav.listopad@gmail.com>
2022-08-17 20:39:42 +0300

    done w/out styles

---
+++ src/components/Register.jsx
@@ -0,0 +1,206 @@
+import React,{useRef,useState,useEffect} from 'react'
+import {faCheck,faTimes,faInfoCircle} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import axios from '../api/axios';
+
+const USER_REGEX = /^[a-z-A-z][a-zA-Z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,23}$/;
+const REGISTER_URL ="/register"
+
+const Register = () => {
+  // ref state
+  const userRef = useRef();
+  const errRef = useRef();
+  // user states
+  const [user,setUser] = useState('');
+  const [validName,setValidName] = useState(false);
+  const [userFocus, setUserFocus] = useState(false);
+
+  // password state
+  const [pwd,setPwd] = useState('');
+  const [validPwd,setValidPwd] = useState(false);
+  const [pwdFocus, setPwdFocus] = useState(false);
+
+  // match password state
+  const [matchPwd,setMatchPwd] = useState('');
+  const [validMatch,setValidMatch] = useState(false);
+  const [matchFocus, setMatchFocus] = useState(false);
+
+  // error state 
+  const [errMsg,setErrMSg] = useState('');
+  const [success,setSuccess] = useState(false)
+
+  useEffect(() => {
+    userRef.current.focus()
+  }, [])
+  
+  useEffect(() => {
+    const result = USER_REGEX.test(user);
+    console.log(result);
+    console.log(user);
+    setValidName(result);
+  }, [user])
+
+  useEffect(() => {
+    const result = PWD_REGEX.test(pwd);
+    console.log(result);
+    console.log(pwd);
+    setValidPwd(result);
+    const match = pwd === matchPwd;
+    setValidMatch(match)
+  }, [pwd,matchPwd])
+
+  useEffect(()=>{
+    setErrMSg('')
+  },[user,pwd,matchPwd])
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    const v1 = USER_REGEX.test(user)
+    const v2 = PWD_REGEX.test(pwd)
+    if(!v1 || !v2){
+      setErrMSg("Invalid entry")
+      return;
+    }
+   try{
+    const response = await axios.post(REGISTER_URL,
+      JSON.stringify({user,pwd}),{
+        headers:{"Content-Type":"application/json"},
+        withCredentials : true,
+      }
+      );
+      console.log(response.data);
+      console.log(response.accessToken);
+      console.log(JSON.stringify(response));
+      setSuccess(true)
+
+   }catch(err){
+    if(!err.response){
+      setErrMSg("No server connection")
+    }else if(err.response.status === 409){
+      setErrMSg("Username Taken")
+    }else{
+      setErrMSg("Failed")
+    }
+    errRef.current.focus()
+
+   }
+  }
+  
+  
+  return (
+    <>
+    {success ? ( 
+      <section>
+        <h1>Success</h1>
+        <p>
+          <a href="#">Sign in</a>
+        </p>
+
+      </section>
+    ):(
+
+    
+    <section>
+      <p ref={errRef} className={errMsg? 'errmsg' : "offscreen"} aria-live='assertive'>
+        {errMsg}
+      </p>
+      <h1>Register form</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">
+          Username:
+          <span className={validName ? "valid" : "hide"}>
+            <FontAwesomeIcon icon ={faCheck} />
+          </span>
+          <span className={validName || !user ? "hide":"invalid"}>   
+          <FontAwesomeIcon icon={faTimes} />          
+          </span>
+        </label>
+        <input 
+        type='text'
+        id='username'
+        ref={userRef}
+        autoComplete ="off"
+        onChange={(e)=>setUser(e.target.value)}
+        required
+        aria-invalid={validName? 'false':'true'}
+        aria-describedby="uidnote"
+        onFocus ={()=>setUserFocus(true)}
+        onBlur = {()=>setUserFocus(false)}
+        />
+        <p id='uidnote' className={userFocus && user && !validName ? 'instrucions' : "offscreen"}>
+          <FontAwesomeIcon icon={faInfoCircle} />
+          4 to 24 characters. <br />
+          Must begin with a letter.<br />
+          Letter,numbers,underscores,hypnens allowed.
+        </p>
+
+        <label htmlFor="password">
+          Password:
+          <span className={validPwd ? "valid": "hide"}>
+            <FontAwesomeIcon icon={faCheck}/>
+          </span>
+          <span className={validPwd || !pwd ? "hide" : "invalid"}>
+            <FontAwesomeIcon  icon={faTimes}/>
+          </span>
+        </label>
+        <input
+        type="password"
+        id="password"
+        onChange={(e)=>setPwd(e.target.value)}
+        required
+        aria-invalid ={validPwd ? "false":"true"}
+        aria-describedby ="pwdnote"
+        onFocus={()=>setPwdFocus(true)}
+        onBlur={()=>setPwdFocus(false)}
+         />
+         <p id="pwdnote" className={pwdFocus && !validPwd  ? "instrucions":"offscreen"}>
+          <FontAwesomeIcon icon={faInfoCircle}/>
+          8 to 24 characters .<br />
+          Must include uppercase and lowercase lettters,number and a special symbot. <br />
+          Allowed special characters .<br />
+         </p>
+
+
+         <label htmlFor="confirm_pwd">
+          Confirm password:
+          <span className={validMatch && matchPwd ? "valid" : "hide"}>
+            <FontAwesomeIcon  icon  ={faCheck}/>
+          </span>
+          <span className={validMatch || !matchPwd ? "hide":"invalid"}>
+            <FontAwesomeIcon icon={faTimes}/>
+          </span>
+         </label>
+         <input 
+         type="password"
+         id="confirm_password"
+         onChange={(e)=>setMatchPwd(e.target.value)}
+         required
+         aria-invalid = {validMatch ? "false" : "true"}
+         aria-describedby='confirmnote'
+         onFocus={()=>setMatchFocus(true)}
+         onBlur = {()=>setMatchFocus(false)}
+         
+         />
+         <p id="confirmnote" className={matchFocus && !validMatch ? "instructions" : "offscreen"}>
+          <FontAwesomeIcon icon={faInfoCircle}/>
+          Must match the password
+
+         </p>
+         <button disabled={!validName || !validPwd || !validMatch ? true : false}>
+          Sign up
+         </button>
+         <p>
+          Already registrated? <br />
+          <span>
+           {/* rout link here */}
+           <a href = "#">Sign in</a>
+          </span>
+         </p>
+      </form>
+    </section>
+    )}
+    </>
+  )
+}
+
+export default Register
